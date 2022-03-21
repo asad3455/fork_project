b2f51b1cf55a01db2bcb8dc9f4eecc8397c828d6
asad3455 <asadmahmood1478@gmail.com>
2022-03-21 17:52:08 +0000

    feat: updated home to show new stream of recipes

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:asad_crew/models/crew.dart';
-import 'package:asad_crew/screens/home/crew_list.dart';
+import 'package:asad_crew/models/recipe.dart';
+import 'package:asad_crew/screens/home/recipe_list.dart';
 import 'package:asad_crew/screens/home/recipe_add.dart';
 import 'package:asad_crew/screens/home/settings_form.dart';
 import 'package:asad_crew/services/auth.dart';
@@ -29,10 +30,10 @@ class Home extends StatelessWidget {
     final AuthService _auth = AuthService();
     //Setting up stream
     //Value of the stream is the crew provider
-    return StreamProvider<List<Crew>?>.value(
+    return StreamProvider<List<Recipe>?>.value(
       //No UID required because crews does not need it
       //We want this stream to be used to wrap the rest of the widget tree
-      value: DatabaseService(uid: "").crews,
+      value: DatabaseService(uid: "").recipes,
       initialData: null,
       child: Scaffold(
         backgroundColor: Color.fromARGB(255, 78, 78, 78),
@@ -64,7 +65,7 @@ class Home extends StatelessWidget {
             ),
           ],
         ),
-        body: CrewList(),
+        body: RecipeList(),
       ),
     );
   }
